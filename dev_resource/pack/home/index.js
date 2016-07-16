083f5115678d745c5b0e35b6c9416e5768245fd9
pengcheng.yang <806321554@qq.com>
2016-07-16 16:15:36 +0800

    debug

--- dev_resource/pack/home/index.js
+++ dev_resource/pack/home/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from "React";
+import React, {Component} from "react";
 import {TopNav, Header} from "../../component/header";
 import Footer from "../../component/footer";
 export class Page extends Component{
