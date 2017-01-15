a5cc0a6b32b798806a373ac391d6a1a6299273c0
pengcheng.yang <806321554@qq.com>
2017-01-15 20:38:37 +0800

    add the query and filter for articles' fetching length

--- controller/api/article.js
+++ controller/api/article.js
@@ -6,47 +6,43 @@ export default [
 		from : "fetch",
 		method : "get",
 		signType : [0, 1, 2],
-		callback(req, res){
-			let length = req.query.length;
-			(async () => {
-				try{
-					res.json({
-						code : 0,
-						data : await Article.find().select("-content"),
-						message : "success"
-					});
-				}catch(err){
-					res.json({
-						code : 400,
-						data : err,
-						message : "failed"
-					});
-				}
-			})();
+		async callback({query}, res){
+			const {length} = query;
+			try{
+				res.json({
+					code : 0,
+					data : await Article.find().select("-content").limit(query.length || 100),
+					message : "success"
+				});
+			}catch(err){
+				res.json({
+					code : 400,
+					data : err,
+					message : "failed"
+				});
+			}
 		}
 	},
 	{
 		from : "fetch/:id",
 		method : "get",
 		signType : [0, 1, 2],
-		callback(req, res){
-			(async () => {
-				try{
-					res.json({
-						code : 0,
-						data : await Article.where({
-							_id : req.params.id
-						}).findOne().select("-__v"),
-						message : "获取文章详情成功"
-					});
-				}catch(err){
-					res.json({
-						code : 400,
-						data : err,
-						message : "no such data searched"
-					});
-				}
-			})();
+		async callback(req, res){
+			try{
+				res.json({
+					code : 0,
+					data : await Article.where({
+						_id : req.params.id
+					}).findOne().select("-__v"),
+					message : "获取文章详情成功"
+				});
+			}catch(err){
+				res.json({
+					code : 400,
+					data : err,
+					message : "no such data searched"
+				});
+			}
 		}
 	}
 ];
